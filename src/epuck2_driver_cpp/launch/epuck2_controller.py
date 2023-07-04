416b2ab5e0e4dedd51d272e44576639e2ca50303
Xinyu-Wu-0000 <xinyu.wuxinyu@outlook.com>
2023-07-04 18:45:50 +0800

    change config to id ip dict

--- src/epuck2_driver_cpp/launch/epuck2_controller.py
+++ src/epuck2_driver_cpp/launch/epuck2_controller.py
@@ -1,13 +1,29 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
+# id2ip = {
+#     4857: "192.168.28.66",
+#     4847: "192.168.28.146",
+#     4791: "192.168.28.250",
+#     5082: "192.168.28.42",
+# }
+id2ip = {
+    4857: "192.168.12.151",
+    4847: "192.168.12.6",
+    4791: "192.168.12.44",
+    5082: "192.168.12.127",
+}
+id_list = []
+ip_list = []
+for (key, value) in id2ip.items():
+    ip_list.append(value)
+    id_list.append(key)
+# init pose x y z, enable cam ,enable ground
+init_list = [[0.0, 0.0, 0.0, False, False]]*ip_list.__len__()
 
-ip_list=["192.168.28.250","192.168.28.66"]
-id_list=[4791,4857]
-init_list=[[0.0,0.0,0.0,False,False], #init pose x y z, enable cam ,enable ground
-           [1.0,1.0,1.0,False,False]]
-if ip_list.__len__()!=id_list.__len__() or ip_list.__len__()!=init_list.__len__():
+if ip_list.__len__() != id_list.__len__() or ip_list.__len__() != init_list.__len__():
     exit(1)
-is_single_robot=1 if ip_list.__len__()==1 else 0
+is_single_robot = 1 if ip_list.__len__() == 1 else 0
+
 
 def generate_launch_description():
     return LaunchDescription([
@@ -19,14 +35,14 @@ def generate_launch_description():
             output="screen",
             emulate_tty=True,
             parameters=[
-                {"epuck2_id":id_list[i]},
-                {"epuck2_address":ip_list[i]},
-                {"epuck2_name":"epuck2_robot_"+str(id_list[i])},
-                {"xpos":init_list[i][0]},
-                {"ypos":init_list[i][1]},
-                {"theta":init_list[i][2]},
-                {"cam_en":init_list[i][3]},
-                {"floor_en":init_list[i][4]}
+                {"epuck2_id": id_list[i]},
+                {"epuck2_address": ip_list[i]},
+                {"epuck2_name": "epuck2_robot_"+str(id_list[i])},
+                {"xpos": init_list[i][0]},
+                {"ypos": init_list[i][1]},
+                {"theta": init_list[i][2]},
+                {"cam_en": init_list[i][3]},
+                {"floor_en": init_list[i][4]}
             ]
         ) for i in range(ip_list.__len__())
     ])
